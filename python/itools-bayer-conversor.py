abb474e0a346940852450f26ecd32236460fb764
Chema Gonzalez <chemag@gmail.com>
2023-03-02 11:18:50 -0800

    itools: bayer-conversor: first version
    Tested:
    
    ```
    $ ./python/itools-bayer-conversor.py --help
    usage: itools-bayer-conversor.py [-h] [-v] [-d] [--quiet]
                              [--i_pix_fmt [RGGB SRGGB8 | GRBG SGRBG8 | GBRG SGBRG8 | BA81 SBGGR8 | RG10 SRGGB10 | BA10 SGRBG10 | GB10 SGBRG10 | BG10 SBGGR10 | pRAA SRGGB10P | pgAA SGRBG10P | pGAA SGBRG10P | pBAA SBGGR10P | aRA8 SRGGB10ALAW8 | aBA8 SBGGR10ALAW8 | aGA8 SGBRG10ALAW8 | agA8 SGRBG10ALAW8 | bRA8 SRGGB10DPCM8 | bBA8 SBGGR10DPCM8 | bGA8 SGBRG10DPCM8 | BD10 SGRBG10DPCM8 | ip3r IPU3_SRGGB10 | ip3b IPU3_SBGGR10 | ip3g IPU3_SGBRG10 | ip3G IPU3_SGRBG10 | RG12 SRGGB12 | BA12 SGRBG12 | GB12 SGBRG12 | BG12 SBGGR12 | pRCC SRGGB12P | pgCC SGRBG12P | pGCC SGBRG12P | pBCC SBGGR12P | RG14 SRGGB14 | GR14 SGRBG14 | GB14 SGBRG14 | BG14 SBGGR14 | pREE SRGGB14P | pgEE SGRBG14P | pGEE SGBRG14P | pBEE SBGGR14P | RG16 SRGGB16 | GR16 SGRBG16 | GB16 SGBRG16 | BA82 SBGGR16 | BYR2 SBGGR16]]
                              [--o_pix_fmt [bayer_bggr8 | bayer_rggb8 | bayer_gbrg8 | bayer_grbg8 | bayer_bggr16be | bayer_rggb16be | bayer_gbrg16be | bayer_grbg16be]] [--width WIDTH] [--height HEIGHT] [--video_size VIDEO_SIZE]
                              [input-file] [output-file]
    
    Module to convert (raw) Bayer (CFA) images to ffmpeg bayer formats. ffmpeg only supports 8 Bayer formats (12 when considering that the 16-bit formats exist in both BE and LE flavors). We want to allow converting other Bayer formats to any of the ffmpeg ones. Main
    goal is to allow ffmpeg access to generic Bayer formats.
    
    positional arguments:
      input-file            input file
      output-file           output file
    
    options:
      -h, --help            show this help message and exit
      -v, --version         Print version
      -d, --debug           Increase verbosity (use multiple times for more)
      --quiet               Zero verbosity
      --i_pix_fmt [RGGB (SRGGB8) | GRBG (SGRBG8) | GBRG (SGBRG8) | BA81 (SBGGR8) | RG10 (SRGGB10) | BA10 (SGRBG10) | GB10 (SGBRG10) | BG10 (SBGGR10) | pRAA (SRGGB10P) | pgAA (SGRBG10P) | pGAA (SGBRG10P) | pBAA (SBGGR10P) | aRA8 (SRGGB10ALAW8) | aBA8 (SBGGR10ALAW8) | aGA8 (SGBRG10ALAW8) | agA8 (SGRBG10ALAW8) | bRA8 (SRGGB10DPCM8) | bBA8 (SBGGR10DPCM8) | bGA8 (SGBRG10DPCM8) | BD10 (SGRBG10DPCM8) | ip3r (IPU3_SRGGB10) | ip3b (IPU3_SBGGR10) | ip3g (IPU3_SGBRG10) | ip3G (IPU3_SGRBG10) | RG12 (SRGGB12) | BA12 (SGRBG12) | GB12 (SGBRG12) | BG12 (SBGGR12) | pRCC (SRGGB12P) | pgCC (SGRBG12P) | pGCC (SGBRG12P) | pBCC (SBGGR12P) | RG14 (SRGGB14) | GR14 (SGRBG14) | GB14 (SGBRG14) | BG14 (SBGGR14) | pREE (SRGGB14P) | pgEE (SGRBG14P) | pGEE (SGBRG14P) | pBEE (SBGGR14P) | RG16 (SRGGB16) | GR16 (SGRBG16) | GB16 (SGBRG16) | BA82 (SBGGR16) | BYR2 (SBGGR16)]
                            input pixel format
      --o_pix_fmt [bayer_bggr8 | bayer_rggb8 | bayer_gbrg8 | bayer_grbg8 | bayer_bggr16be | bayer_rggb16be | bayer_gbrg16be | bayer_grbg16be]
                            output pixel format
      --width WIDTH         use WIDTH width (default: 0)
      --height HEIGHT       HEIGHT height (default: 0)
      --video_size VIDEO_SIZE
                            use <width>x<height>
    
    ```

---
+++ python/itools-bayer-conversor.py
@@ -0,0 +1,791 @@
+#!/usr/bin/env python3
+
+"""Module to convert (raw) Bayer (CFA) images to ffmpeg bayer formats.
+
+ffmpeg only supports 8 Bayer formats (12 when considering that the 16-bit
+formats exist in both BE and LE flavors). We want to allow converting
+other Bayer formats to any of the ffmpeg ones. Main goal is to allow
+ffmpeg access to generic Bayer formats.
+"""
+
+
+import argparse
+import sys
+
+
+__version__ = "0.1"
+
+COLOR_ORDER = ["RGGB", "BGGR", "GRBG", "GBRG"]
+
+
+# read functions
+
+# 2 bytes -> 2 components
+def rfun_8(data):
+    return data[0], data[1]
+
+
+# 4 bytes -> 2 components
+def rfun_10_expanded_to_16(data):
+    # check the high 6 bits of both components are 0x0
+    if (data[1] & 0xFC) != 0 or (data[3] & 0xFC) != 0:
+        print("warning: upper 6 bits are not zero")
+    return (
+        (data[0] << 6) | ((data[1] & 0x03) << 14),
+        (data[2] << 6) | ((data[3] & 0x03) << 14),
+    )
+
+
+# 5 bytes -> 4 components
+def rfun_10_packed(data):
+    low = data[4]
+    return (
+        (data[0] << 8) | ((low & 0x03) << 6),
+        (data[1] << 8) | ((low & 0x0C) << 4),
+        (data[2] << 8) | ((low & 0x30) << 2),
+        (data[3] << 8) | ((low & 0xC0) << 0),
+    )
+
+
+# 2 bytes -> 2 components
+def rfun_10_alaw(data):
+    raise AssertionError("rfun_10_alaw: unimplented")
+
+
+# 2 bytes -> 2 components
+def rfun_10_dpcm(data):
+    raise AssertionError("rfun_10_dpcm: unimplented")
+
+
+# 32 bytes -> 25 components
+def rfun_10_ipu3(data):
+    raise AssertionError("rfun_10_ipu3: unimplented")
+
+
+# 4 bytes -> 2 components
+def rfun_12_expanded_to_16(data):
+    # check the high 4 bits of both components are 0x0
+    if (data[1] & 0xF0) != 0 or (data[3] & 0xF0) != 0:
+        print("warning: upper 4 bits are not zero")
+    return (
+        (data[0] << 4) | ((data[1] & 0x0F) << 12),
+        (data[2] << 4) | ((data[3] & 0x0F) << 12),
+    )
+
+
+# 3 bytes -> 2 components
+def rfun_12_packed(data):
+    low = data[2]
+    return (
+        (data[0] << 8) | ((low & 0x0F) << 4),
+        (data[1] << 8) | ((low & 0xF0) << 0),
+    )
+
+
+# 4 bytes -> 2 components
+def rfun_14_expanded_to_16(data):
+    # check the high 2 bits of both components are 0x0
+    if (data[1] & 0xC0) != 0 or (data[3] & 0xC0) != 0:
+        print("warning: upper 2 bits are not zero")
+    return (
+        (data[0] << 2) | ((data[1] & 0x3F) << 10),
+        (data[2] << 2) | ((data[3] & 0x3F) << 10),
+    )
+
+
+# 7 bytes -> 4 components
+def rfun_14_packed(data):
+    low0, low1, low2 = data[4:6]
+    return (
+        (data[0] << 8) | ((low0 & 0x3F) << 2),
+        (data[1] << 8) | ((low1 & 0x0F) << 2) | ((low0 & 0xC0) << 0),
+        (data[2] << 8) | ((low2 & 0x03) << 2) | ((low1 & 0xF0) << 0),
+        (data[3] << 8) | ((low2 & 0xFC) << 0),
+    )
+
+
+# 4 bytes -> 2 components
+def rfun_16(data):
+    return (
+        (data[0] << 0) | (data[1] << 8),
+        (data[2] << 0) | (data[3] << 8),
+    )
+
+
+# write functions
+
+# 2 bytes -> 2 components
+def wfun_8(c0, c1):
+    raise AssertionError("wfun_8: unimplented")
+    return c0.to_bytes(1, "big") + c1.to_bytes(2, "big")
+
+
+# 4 bytes -> 2 components
+def wfun_16be(c0, c1):
+    return c0.to_bytes(2, "big") + c1.to_bytes(2, "big")
+
+
+INPUT_FORMATS = {
+    # 8-bit Bayer formats
+    "RGGB": {
+        "alias": "SRGGB8",
+        # byte length
+        "blen": 2,
+        # component length
+        "clen": 2,
+        # component depth (in bits)
+        "cdepth": 8,
+        # read function
+        "rfun": rfun_8,
+        # component order
+        "order": "RGGB",
+    },
+    "GRBG": {
+        "alias": "SGRBG8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 8,
+        "rfun": rfun_8,
+        "order": "GRBG",
+    },
+    "GBRG": {
+        "alias": "SGBRG8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 8,
+        "rfun": rfun_8,
+        "order": "GBRG",
+    },
+    "BA81": {
+        "alias": "SBGGR8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 8,
+        "rfun": rfun_8,
+        "order": "BGGR",
+    },
+    # 10-bit Bayer formats expanded to 16 bits
+    "RG10": {
+        "alias": "SRGGB10",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_expanded_to_16,
+        "order": "RGGB",
+    },
+    "BA10": {
+        "alias": "SGRBG10",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_expanded_to_16,
+        "order": "GRBG",
+    },
+    "GB10": {
+        "alias": "SGBRG10",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_expanded_to_16,
+        "order": "GBRG",
+    },
+    "BG10": {
+        "alias": "SBGGR10",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_expanded_to_16,
+        "order": "BGGR",
+    },
+    # 10-bit Bayer formats (packed)
+    "pRAA": {
+        "alias": "SRGGB10P",
+        "blen": 5,
+        "clen": 4,
+        "cdepth": 16,
+        "rfun": rfun_10_packed,
+        "order": "RGGB",
+    },
+    "pgAA": {
+        "alias": "SGRBG10P",
+        "blen": 5,
+        "clen": 4,
+        "cdepth": 16,
+        "rfun": rfun_10_packed,
+        "order": "GRBG",
+    },
+    "pGAA": {
+        "alias": "SGBRG10P",
+        "blen": 5,
+        "clen": 4,
+        "cdepth": 16,
+        "rfun": rfun_10_packed,
+        "order": "GBRG",
+    },
+    "pBAA": {
+        "alias": "SBGGR10P",
+        "blen": 5,
+        "clen": 4,
+        "cdepth": 16,
+        "rfun": rfun_10_packed,
+        "order": "BGGR",
+    },
+    # 10-bit Bayer formats compressed to 8 bits using a-law
+    "aRA8": {
+        "alias": "SRGGB10ALAW8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_alaw,
+        "order": "RGGB",
+    },
+    "aBA8": {
+        "alias": "SBGGR10ALAW8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_alaw,
+        "order": "BGGR",
+    },
+    "aGA8": {
+        "alias": "SGBRG10ALAW8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_alaw,
+        "order": "GBRG",
+    },
+    "agA8": {
+        "alias": "SGRBG10ALAW8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_alaw,
+        "order": "GRBG",
+    },
+    # 10-bit Bayer formats compressed to 8 bits using dpcm
+    "bRA8": {
+        "alias": "SRGGB10DPCM8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_dpcm,
+        "order": "RGGB",
+    },
+    "bBA8": {
+        "alias": "SBGGR10DPCM8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_dpcm,
+        "order": "BGGR",
+    },
+    "bGA8": {
+        "alias": "SGBRG10DPCM8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_dpcm,
+        "order": "GBRG",
+    },
+    "BD10": {
+        "alias": "SGRBG10DPCM8",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_10_dpcm,
+        "order": "GRBG",
+    },
+    # 10-bit Bayer formats compressed a la Intel IPU3 driver
+    "ip3r": {
+        "alias": "IPU3_SRGGB10",
+        "blen": 32,
+        "clen": 25,
+        "cdepth": 16,
+        "rfun": rfun_10_ipu3,
+        "order": "RGGB",
+    },
+    "ip3b": {
+        "alias": "IPU3_SBGGR10",
+        "blen": 32,
+        "clen": 25,
+        "cdepth": 16,
+        "rfun": rfun_10_ipu3,
+        "order": "BGGR",
+    },
+    "ip3g": {
+        "alias": "IPU3_SGBRG10",
+        "blen": 32,
+        "clen": 25,
+        "cdepth": 16,
+        "rfun": rfun_10_ipu3,
+        "order": "GBRG",
+    },
+    "ip3G": {
+        "alias": "IPU3_SGRBG10",
+        "blen": 32,
+        "clen": 25,
+        "cdepth": 16,
+        "rfun": rfun_10_ipu3,
+        "order": "GRBG",
+    },
+    # 12-bit Bayer formats expanded to 16 bits
+    "RG12": {
+        "alias": "SRGGB12",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_12_expanded_to_16,
+        "order": "RGGB",
+    },
+    "BA12": {
+        "alias": "SGRBG12",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_12_expanded_to_16,
+        "order": "GRBG",
+    },
+    "GB12": {
+        "alias": "SGBRG12",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_12_expanded_to_16,
+        "order": "GBRG",
+    },
+    "BG12": {
+        "alias": "SBGGR12",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_12_expanded_to_16,
+        "order": "BGGR",
+    },
+    # 12-bit Bayer formats (packed)
+    "pRCC": {
+        "alias": "SRGGB12P",
+        "blen": 3,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_12_packed,
+        "order": "RGGB",
+    },
+    "pgCC": {
+        "alias": "SGRBG12P",
+        "blen": 3,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_12_packed,
+        "order": "GRBG",
+    },
+    "pGCC": {
+        "alias": "SGBRG12P",
+        "blen": 3,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_12_packed,
+        "order": "GBRG",
+    },
+    "pBCC": {
+        "alias": "SBGGR12P",
+        "blen": 3,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_12_packed,
+        "order": "BGGR",
+    },
+    # 14-bit Bayer formats expanded to 16 bits
+    "RG14": {
+        "alias": "SRGGB14",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_14_expanded_to_16,
+        "order": "RGGB",
+    },
+    "GR14": {
+        "alias": "SGRBG14",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_14_expanded_to_16,
+        "order": "GRBG",
+    },
+    "GB14": {
+        "alias": "SGBRG14",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_14_expanded_to_16,
+        "order": "GBRG",
+    },
+    "BG14": {
+        "alias": "SBGGR14",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_14_expanded_to_16,
+        "order": "BGGR",
+    },
+    # 14-bit Bayer formats (packed)
+    "pREE": {
+        "alias": "SRGGB14P",
+        "blen": 7,
+        "clen": 4,
+        "cdepth": 16,
+        "rfun": rfun_14_packed,
+        "order": "RGGB",
+    },
+    "pgEE": {
+        "alias": "SGRBG14P",
+        "blen": 7,
+        "clen": 4,
+        "cdepth": 16,
+        "rfun": rfun_14_packed,
+        "order": "GRBG",
+    },
+    "pGEE": {
+        "alias": "SGBRG14P",
+        "blen": 7,
+        "clen": 4,
+        "cdepth": 16,
+        "rfun": rfun_14_packed,
+        "order": "GBRG",
+    },
+    "pBEE": {
+        "alias": "SBGGR14P",
+        "blen": 7,
+        "clen": 4,
+        "cdepth": 16,
+        "rfun": rfun_14_packed,
+        "order": "BGGR",
+    },
+    # 16-bit Bayer formats
+    "RG16": {
+        "alias": "SRGGB16",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_16,
+        "order": "RGGB",
+    },
+    "GR16": {
+        "alias": "SGRBG16",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_16,
+        "order": "GRBG",
+    },
+    "GB16": {
+        "alias": "SGBRG16",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_16,
+        "order": "GBRG",
+    },
+    "BA82": {
+        "alias": "SBGGR16",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_16,
+        "order": "BGGR",
+    },
+    "BYR2": {
+        "alias": "SBGGR16",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "rfun": rfun_16,
+        "order": "BGGR",
+    },
+}
+
+
+OUTPUT_FORMATS = {
+    "bayer_bggr8": {
+        "order": "BGGR",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 8,
+        # write function
+        "wfun": wfun_8,
+    },
+    "bayer_rggb8": {
+        "order": "RGGB",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 8,
+        "wfun": wfun_8,
+    },
+    "bayer_gbrg8": {
+        "order": "GBRG",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 8,
+        "wfun": wfun_8,
+    },
+    "bayer_grbg8": {
+        "order": "GRBG",
+        "blen": 2,
+        "clen": 2,
+        "cdepth": 8,
+        "wfun": wfun_8,
+    },
+    "bayer_bggr16be": {
+        "order": "BGGR",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "wfun": wfun_16be,
+    },
+    "bayer_rggb16be": {
+        "order": "RGGB",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "wfun": wfun_16be,
+    },
+    "bayer_gbrg16be": {
+        "order": "GBRG",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "wfun": wfun_16be,
+    },
+    "bayer_grbg16be": {
+        "order": "GRBG",
+        "blen": 4,
+        "clen": 2,
+        "cdepth": 16,
+        "wfun": wfun_16be,
+    },
+}
+
+
+default_values = {
+    "debug": 0,
+    "dry_run": False,
+    "i_pix_fmt": None,
+    "o_pix_fmt": None,
+    "width": 0,
+    "height": 0,
+    "infile": None,
+    "outfile": None,
+}
+
+
+# for Bayer pixel formats, only the width is important
+def rfun_image_file(infile, i_pix_fmt, width, height, outfile, o_pix_fmt, debug):
+    # convert alias input pixel formats to the canonical names
+    if i_pix_fmt in (v["alias"] for v in INPUT_FORMATS.values()):
+        # find the canonical name
+        for canonical, v in INPUT_FORMATS.items():
+            if i_pix_fmt == v["alias"]:
+                i_pix_fmt = canonical
+
+    # check which output pixel format is recommended
+    # * size: all conversions are valid (including those that do 10+ -> 8 bits)
+    # * order: we enforce the same component order to make the code simpler
+    cdepth = INPUT_FORMATS[i_pix_fmt]["cdepth"]
+    iorder = INPUT_FORMATS[i_pix_fmt]["order"]
+    for pix_fmt, v in OUTPUT_FORMATS.items():
+        if v["cdepth"] == cdepth and v["order"] == iorder:
+            expected_o_pix_fmt = pix_fmt
+            break
+    else:
+        raise AssertionError(f"error: no match for input pix_fmt: {i_pix_fmt}")
+    if o_pix_fmt is None:
+        o_pix_fmt = expected_o_pix_fmt
+        print(f"using {o_pix_fmt} as output pixel format")
+    else:
+        assert expected_o_pix_fmt == o_pix_fmt, (
+            f"error: {expected_o_pix_fmt} is preferred to {o_pix_fmt} as "
+            "output pixel format"
+        )
+
+    # ifmt = INPUT_FORMATS[i_pix_fmt]  # XXX
+    # ofmt = OUTPUT_FORMATS[o_pix_fmt]  # XXX
+
+    # open infile and outfile
+    with open(infile, "rb") as fin, open(outfile, "wb") as fout:
+        # process infile
+        while True:
+            # row_index = 0
+            idata = fin.read(INPUT_FORMATS[i_pix_fmt]["blen"])
+            if not idata:
+                break
+            components = INPUT_FORMATS[i_pix_fmt]["rfun"](idata)
+            c_index = 0
+            while c_index < len(components):
+                clen = OUTPUT_FORMATS[o_pix_fmt]["clen"]
+                odata = OUTPUT_FORMATS[o_pix_fmt]["wfun"](
+                    *components[c_index : c_index + clen]
+                )
+                fout.write(odata)
+                c_index += clen
+
+            # TODO(chema): enforce input width (width % iclen == 0)
+            # row_index = 0
+
+        print(
+            f"ffmpeg -f rawvideo -pixel_format {o_pix_fmt} "
+            f"-s {width}x{height} -i {outfile} {outfile}.png"
+        )
+
+
+def get_options(argv):
+    """Generic option parser.
+
+    Args:
+        argv: list containing arguments
+
+    Returns:
+        Namespace - An argparse.ArgumentParser-generated option object
+    """
+    # init parser
+    # usage = 'usage: %prog [options] arg1 arg2'
+    # parser = argparse.OptionParser(usage=usage)
+    # parser.print_help() to get argparse.usage (large help)
+    # parser.print_usage() to get argparse.usage (just usage line)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        dest="version",
+        default=False,
+        help="Print version",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="count",
+        dest="debug",
+        default=default_values["debug"],
+        help="Increase verbosity (use multiple times for more)",
+    )
+    parser.add_argument(
+        "--quiet",
+        action="store_const",
+        dest="debug",
+        const=-1,
+        help="Zero verbosity",
+    )
+    input_choices = list(INPUT_FORMATS.keys()) + list(INPUT_FORMATS.values())
+    input_choices_str = " | ".join(
+        f'{k} ({v["alias"]})'
+        for k, v in zip(INPUT_FORMATS.keys(), INPUT_FORMATS.values())
+    )
+    parser.add_argument(
+        "--i_pix_fmt",
+        action="store",
+        type=str,
+        dest="i_pix_fmt",
+        default=default_values["i_pix_fmt"],
+        choices=input_choices,
+        metavar=f"[{input_choices_str}]",
+        help="input pixel format",
+    )
+    parser.add_argument(
+        "--o_pix_fmt",
+        action="store",
+        type=str,
+        dest="o_pix_fmt",
+        default=default_values["o_pix_fmt"],
+        choices=OUTPUT_FORMATS.keys(),
+        metavar="[%s]"
+        % (
+            " | ".join(
+                OUTPUT_FORMATS.keys(),
+            )
+        ),
+        help="output pixel format",
+    )
+    # 2-parameter setter using argparse.Action
+    parser.add_argument(
+        "--width",
+        action="store",
+        type=int,
+        dest="width",
+        default=default_values["width"],
+        metavar="WIDTH",
+        help=("use WIDTH width (default: %i)" % default_values["width"]),
+    )
+    parser.add_argument(
+        "--height",
+        action="store",
+        type=int,
+        dest="height",
+        default=default_values["height"],
+        metavar="HEIGHT",
+        help=("HEIGHT height (default: %i)" % default_values["height"]),
+    )
+
+    class VideoSizeAction(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            namespace.width, namespace.height = [int(v) for v in values[0].split("x")]
+
+    parser.add_argument(
+        "--video_size",
+        action=VideoSizeAction,
+        nargs=1,
+        help="use <width>x<height>",
+    )
+
+    parser.add_argument(
+        "infile",
+        type=str,
+        nargs="?",
+        default=default_values["infile"],
+        metavar="input-file",
+        help="input file",
+    )
+    parser.add_argument(
+        "outfile",
+        type=str,
+        nargs="?",
+        default=default_values["outfile"],
+        metavar="output-file",
+        help="output file",
+    )
+    # do the parsing
+    options = parser.parse_args(argv[1:])
+    if options.version:
+        return options
+    return options
+
+
+def main(argv):
+    # parse options
+    options = get_options(argv)
+    if options.version:
+        print("version: %s" % __version__)
+        sys.exit(0)
+
+    # get infile/outfile
+    if options.infile == "-":
+        options.infile = "/dev/fd/0"
+    if options.outfile == "-":
+        options.outfile = "/dev/fd/1"
+    # print results
+    if options.debug > 0:
+        print(options)
+    # convert input image file
+    rfun_image_file(
+        options.infile,
+        options.i_pix_fmt,
+        options.width,
+        options.height,
+        options.outfile,
+        options.o_pix_fmt,
+        options.debug,
+    )
+
+
+if __name__ == "__main__":
+    # at least the CLI program name: (CLI) execution
+    main(sys.argv)
