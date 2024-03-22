d0382b18304a72d97317576d8ed8eb1a4a229487
Chema Gonzalez <chemag@meta.com>
2024-03-22 09:37:40 -0700

    itools: move run() to common file

---
+++ python/itools-common.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""itools-common.py module description.
+
+
+Module that contains common code.
+"""
+
+
+import enum
+import subprocess
+import sys
+
+
+def run(command, **kwargs):
+    debug = kwargs.get("debug", 0)
+    dry_run = kwargs.get("dry_run", False)
+    env = kwargs.get("env", None)
+    stdin = subprocess.PIPE if kwargs.get("stdin", False) else None
+    bufsize = kwargs.get("bufsize", 0)
+    universal_newlines = kwargs.get("universal_newlines", False)
+    default_close_fds = True if sys.platform == "linux2" else False
+    close_fds = kwargs.get("close_fds", default_close_fds)
+    shell = type(command) in (type(""), type(""))
+    if debug > 0:
+        print(f"running $ {command}")
+    if dry_run:
+        return 0, b"stdout", b"stderr"
+    p = subprocess.Popen(  # noqa: E501
+        command,
+        stdin=stdin,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        bufsize=bufsize,
+        universal_newlines=universal_newlines,
+        env=env,
+        close_fds=close_fds,
+        shell=shell,
+    )
+    # wait for the command to terminate
+    if stdin is not None:
+        out, err = p.communicate(stdin)
+    else:
+        out, err = p.communicate()
+    returncode = p.returncode
+    # clean up
+    del p
+    # return results
+    return returncode, out, err
--- python/itools-pattern.py
+++ python/itools-pattern.py
@@ -12,6 +12,9 @@ import argparse
 import numpy as np
 import subprocess
 import sys
+import importlib
+
+itools_common = importlib.import_module("itools-common")
 
 
 PATTERN_LIST = ("kwrgb", "bandw", "grayscale")
@@ -92,43 +95,6 @@ PATTERN_COLORS = {
     "kwrgb": ("black64", "white", "red", "green", "blue"),
     "grayscale": ("black0", "black64", "gray100", "gray200", "gray300", "white"),
 }
-
-
-def run(command, **kwargs):
-    debug = kwargs.get("debug", 0)
-    dry_run = kwargs.get("dry_run", False)
-    env = kwargs.get("env", None)
-    stdin = subprocess.PIPE if kwargs.get("stdin", False) else None
-    bufsize = kwargs.get("bufsize", 0)
-    universal_newlines = kwargs.get("universal_newlines", False)
-    default_close_fds = True if sys.platform == "linux2" else False
-    close_fds = kwargs.get("close_fds", default_close_fds)
-    shell = type(command) in (type(""), type(""))
-    if debug > 0:
-        print(f"running $ {command}")
-    if dry_run:
-        return 0, b"stdout", b"stderr"
-    p = subprocess.Popen(  # noqa: E501
-        command,
-        stdin=stdin,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        bufsize=bufsize,
-        universal_newlines=universal_newlines,
-        env=env,
-        close_fds=close_fds,
-        shell=shell,
-    )
-    # wait for the command to terminate
-    if stdin is not None:
-        out, err = p.communicate(stdin)
-    else:
-        out, err = p.communicate()
-    returncode = p.returncode
-    # clean up
-    del p
-    # return results
-    return returncode, out, err
 
 
 def read_yuv420p10le_to_ndarray(infile, num_cols, num_rows):
@@ -283,7 +249,7 @@ def diff(
     # write the diff as a png file
     outfile_png = f"{outfile}.png"
     command = f"ffmpeg -y -f rawvideo -pixel_format yuv420p10le -s {num_cols}x{num_rows} -i {outfile} {outfile_png}"
-    run(command, debug=debug)
+    itools_common.run(command, debug=debug)
     print(f"output: {outfile} png: {outfile_png}")
 
 
