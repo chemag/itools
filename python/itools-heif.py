613e038d0256926afc7618c7def86af384b28121
Chema Gonzalez <chemag@meta.com>
2024-03-23 08:58:59 -0700

    itools: heif: add h265 colorimetry
    Tested:
    ```
    $ ./itools-analysis.py *heic | csvlook
    | filename                  | ymean | ystddev | umean | ustddev | vmean | vstddev | rmean | rstddev | gmean | gstddev | bmean | bstddev | status                                        |
    | ------------------------- | ----- | ------- | ----- | ------- | ----- | ------- | ----- | ------- | ----- | ------- | ----- | ------- | --------------------------------------------- |
    | black.png.heic-enc.heic   |     0 |       0 |   128 |       0 |   128 |       0 |     0 |       0 |     0 |       0 |     0 |       0 | colorrange=full|broken=0|fr=1|cp=1|tc=13|mc=6 |
    | blue.png.heic-enc.heic    |    29 |       0 |   255 |       0 |   107 |       0 |     0 |       0 |     0 |       0 |   254 |       0 | colorrange=full|broken=0|fr=1|cp=1|tc=13|mc=6 |
    | gray128.png.heic-enc.heic |   128 |       0 |   128 |       0 |   128 |       0 |   128 |       0 |   128 |       0 |   128 |       0 | colorrange=full|broken=0|fr=1|cp=1|tc=13|mc=6 |
    | green.png.heic-enc.heic   |   150 |       0 |    44 |       0 |    21 |       0 |     0 |       0 |   255 |       0 |     1 |       0 | colorrange=full|broken=0|fr=1|cp=1|tc=13|mc=6 |
    | red.png.heic-enc.heic     |    76 |       0 |    85 |       0 |   255 |       0 |   254 |       0 |     0 |       0 |     0 |       0 | colorrange=full|broken=0|fr=1|cp=1|tc=13|mc=6 |
    | white.png.heic-enc.heic   |   255 |       0 |   128 |       0 |   128 |       0 |   255 |       0 |   255 |       0 |   255 |       0 | colorrange=full|broken=0|fr=1|cp=1|tc=13|mc=6 |
    ```

--- python/itools-heif.py
+++ python/itools-heif.py
@@ -2,16 +2,98 @@
 
 """itools-heif.py module description.
 
-Runs generic HEIF analysis. Requires access to heif-convert (libheif) and ffmpeg.
+Runs generic HEIF analysis. Requires access to heif-convert (libheif), MP4Box (gpac), and ffmpeg.
 """
-# https://docs.opencv.org/3.4/d4/d61/tutorial_warp_affine.html
 
 
 import importlib
+import pandas as pd
+import re
 import tempfile
 
 itools_common = importlib.import_module("itools-common")
 itools_y4m = importlib.import_module("itools-y4m")
+
+
+def parse_item_list(output):
+    primary_id = None
+    df = pd.DataFrame(columns=("item_id", "id", "type", "size", "rem", "primary"))
+    for line in output.decode("ascii").split("\n"):
+        if line.startswith("Primary Item - ID "):
+            primary_id = int(line[len("Primary Item - ID ") :])
+        elif line.startswith("Item #"):
+            # 'Item #1: ID 10000 type hvc1 size 512x512 Hidden'
+            pattern = (
+                r"Item #(?P<item_id>\d+): ID (?P<id>\d+) type (?P<type>\w*)(?P<rem>.*)"
+            )
+            res = re.search(pattern, line)
+            if not res:
+                print(f'error: regexp does not work with line "{line}"')
+            item_id = int(res.group("item_id"))
+            the_id = int(res.group("id"))
+            the_type = res.group("type")
+            rem = res.group("rem")
+            if rem:
+                pattern = r" *size (?P<size>\S*)(?P<rem>.*)"
+                res = re.search(pattern, line)
+                size = res.group("size")
+                rem = res.group("rem")
+            else:
+                size = None
+            df.loc[df.size] = [
+                item_id,
+                the_id,
+                the_type,
+                size,
+                rem,
+                the_id == primary_id,
+            ]
+    return df
+
+
+def get_item_list(infile, debug=0):
+    # get the item list
+    command = f"MP4Box -info {infile}"
+    returncode, out, err = itools_common.run(command, debug=debug)
+    assert returncode == 0, f"error in {command}\n{err}"
+    return parse_item_list(err)
+
+
+COLOR_PARAMETER_LIST = {
+    "colour_primaries": "cp",
+    "transfer_characteristics": "tc",
+    "matrix_coefficients": "mc",
+    "video_full_range_flag": "fr",
+}
+
+
+def parse_ffmpeg_bsf_colorimetry(output):
+    colorimetry = {}
+    for line in output.decode("ascii").split("\n"):
+        for color_parameter in COLOR_PARAMETER_LIST.keys():
+            if color_parameter in line:
+                rem = line[line.index(color_parameter) + len(color_parameter) :].strip()
+                colorimetry[COLOR_PARAMETER_LIST[color_parameter]] = int(
+                    rem.split("=")[1]
+                )
+    return colorimetry
+
+
+def get_h265_colorimetry(infile, debug):
+    df_item = get_item_list(infile, debug)
+    # select the first hvc1 type
+    the_id = df_item[df_item.type == "hvc1"]["id"][0]
+    # extract the 265 file of the first tile
+    tmp265 = tempfile.NamedTemporaryFile(suffix=".265").name
+    command = f"MP4Box -dump-item {the_id}:path={tmp265} {infile}"
+    returncode, out, err = itools_common.run(command, debug=debug)
+    assert returncode == 0, f"error in {command}\n{err}"
+    # extract the 265 file of the first tile
+    command = f"ffmpeg -i {tmp265} -c:v copy -bsf:v trace_headers -f null -"
+    returncode, out, err = itools_common.run(command, debug=debug)
+    assert returncode == 0, f"error in {command}\n{err}"
+    colorimetry = parse_ffmpeg_bsf_colorimetry(err)
+    return colorimetry
 
 
 def read_heif(infile, debug=0):
@@ -29,4 +111,7 @@ def read_heif(infile, debug=0):
     assert returncode == 0, f"error in {command}\n{err}"
     # read the y4m frame
     outyvu, _, _, status = itools_y4m.read_y4m(tmpy4m2, colorrange="full", debug=debug)
+    # get the h265 colorimetry
+    colorimetry = get_h265_colorimetry(infile, debug)
+    status.update(colorimetry)
     return outyvu, status
