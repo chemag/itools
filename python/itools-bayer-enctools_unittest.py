9e6111ba71474b78361b5c835203af7fde674ecd
Chema Gonzalez <chemag@meta.com>
2025-03-25 13:54:38 -0700

    itools: add tests for bayer encoding tool

---
+++ python/itools-bayer-enctools_unittest.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+"""itools-bayer-enctool_unittest.py: itools bayer enctools unittest.
+
+# runme
+# $ ./itools-bayer-enctool_unittest.py
+"""
+
+import importlib
+import math
+import os
+import shlex
+import string
+import tempfile
+import numpy as np
+import unittest
+
+itools_version = importlib.import_module("itools-version")
+itools_bayer_enctool = importlib.import_module("itools-bayer-enctools")
+
+
+convertRg1g2bToYdgcocgTestCases = [
+    {
+        "name": "basic-4x4",
+        "bayer_image": np.array(
+            [
+                0x00,
+                0x40,
+                0x10,
+                0x30,
+                0x80,
+                0xFF,
+                0x90,
+                0xF0,
+                0x20,
+                0x20,
+                0x30,
+                0x10,
+                0xA0,
+                0xE0,
+                0xB0,
+                0xD0,
+            ],
+            dtype=np.uint8,
+        ).reshape(4, 4),
+        "bayer_y": np.array([0x70, 0x70, 0x70, 0x70], dtype=np.uint8).reshape(2, 2),
+        "bayer_dg": np.array([0xA0, 0xB0, 0xC0, 0xD0], dtype=np.uint8).reshape(2, 2),
+        "bayer_co": np.array([0x00, 0x10, 0x20, 0x30], dtype=np.uint8).reshape(2, 2),
+        "bayer_cg": np.array([0x70, 0x70, 0x70, 0x70], dtype=np.uint8).reshape(2, 2),
+    },
+    {
+        "name": "reverse-4x4",
+        "bayer_image": np.array(
+            [
+                0x00,
+                0x40,
+                0x10,
+                0x30,
+                0x80,
+                0x00,
+                0x90,
+                0xF0,
+                0x20,
+                0x20,
+                0x30,
+                0x10,
+                0xA0,
+                0xE0,
+                0xB0,
+                0xD0,
+            ],
+            dtype=np.uint8,
+        ).reshape(4, 4),
+        "bayer_y": np.array([0x30, 0x70, 0x70, 0x70], dtype=np.uint8).reshape(2, 2),
+        "bayer_dg": np.array([0xA0, 0xB0, 0xC0, 0xD0], dtype=np.uint8).reshape(2, 2),
+        "bayer_co": np.array([0x80, 0x10, 0x20, 0x30], dtype=np.uint8).reshape(2, 2),
+        "bayer_cg": np.array([0xB0, 0x70, 0x70, 0x70], dtype=np.uint8).reshape(2, 2),
+    },
+]
+
+
+class MainTest(unittest.TestCase):
+    def testConvertRg1g2bToYdgcocg(self):
+        """convert_rg1g2b_to_ydgcocg test."""
+        for test_case in convertRg1g2bToYdgcocgTestCases:
+            print("...running %s" % test_case["name"])
+            # 1. run RG1G2B to YDgCoCg function
+            bayer_y, bayer_dg, bayer_co, bayer_cg = (
+                itools_bayer_enctool.convert_rg1g2b_to_ydgcocg(test_case["bayer_image"])
+            )
+            # check the values
+            np.testing.assert_array_equal(test_case["bayer_y"], bayer_y)
+            np.testing.assert_array_equal(test_case["bayer_dg"], bayer_dg)
+            np.testing.assert_array_equal(test_case["bayer_co"], bayer_co)
+            np.testing.assert_array_equal(test_case["bayer_cg"], bayer_cg)
+            # 2. run YDgCoCg to RG1G2B function (reverse)
+            bayer_image = itools_bayer_enctool.convert_ydgcocg_to_rg1g2b(
+                bayer_y, bayer_dg, bayer_co, bayer_cg
+            )
+            np.testing.assert_array_equal(test_case["bayer_image"], bayer_image)
+
+
+if __name__ == "__main__":
+    unittest.main()
