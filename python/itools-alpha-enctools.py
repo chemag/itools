25067b71c111c86eac1cafdbb36339d2287419fd
Chema Gonzalez <chemag@meta.com>
2025-05-24 09:40:37 -0700

    itools: alpha: add encoder test tool
    Tested:
    ```
    $ ./itools-alpha-enctools.py y4m/*y4m  |csvcut  -C infile | csvlook -I
    | width  | height | depth | codec   | raw_size  | encoded_size | encoded_bpp         | encoded_cr         |
    | ------ | ------ | ----- | ------- | --------- | ------------ | ------------------- | ------------------ |
    | 1152.0 | 1152.0 | 8.0   | default | 1327169.0 | 45919.0      | 0.27680724344135804 | 28.902393344802803 |
    | 1125.0 | 910.0  | 8.0   | default | 1023820.0 | 35281.0      | 0.2757001221001221  | 29.01901873529662  |
    | 420.0  | 429.0  | 8.0   | default | 180249.0  | 183721.0     | 8.157220557220557   | 0.9811017793284382 |
    | 728.0  | 543.0  | 8.0   | default | 395373.0  | 33448.0      | 0.6769068868516382  | 11.820527385792872 |
    | 640.0  | 480.0  | 8.0   | default | 307263.0  | 40525.0      | 1.0553385416666667  | 7.582060456508328  |
    | 640.0  | 480.0  | 8.0   | default | 307263.0  | 35789.0      | 0.9320052083333333  | 8.585403336220626  |
    | 1113.0 | 900.0  | 8.0   | default | 1001770.0 | 187601.0     | 1.498260956374164   | 5.339896908865091  |
    | 368.0  | 540.0  | 8.0   | default | 198783.0  | 1707.0       | 0.06871980676328503 | 116.45166959578208 |
    | 540.0  | 721.0  | 8.0   | default | 389409.0  | 99304.0      | 2.040458211331998   | 3.921382824458229  |
    | 428.0  | 784.0  | 8.0   | default | 335621.0  | 170824.0     | 4.072668319664315   | 1.9647180724020044 |
    | 715.4  | 693.9  | 8.0   | default | 546672.0  | 83411.9      | 1.9054085853747438  | 21.456817243945704 |
    ```

---
+++ python/itools-alpha-enctools.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python3
+
+"""itools-alpha-enctool.py module description.
+
+This is a tool to test Alpha image encoding.
+"""
+
+
+import argparse
+import cv2
+import importlib
+import io
+import itertools
+import json
+import math
+import numpy as np
+import os
+import pandas as pd
+import PIL
+import pillow_heif
+import re
+import shutil
+import sys
+import tempfile
+
+itools_version = importlib.import_module("itools-version")
+itools_common = importlib.import_module("itools-common")
+itools_alpha = importlib.import_module("itools-alpha")
+itools_y4m = importlib.import_module("itools-y4m")
+
+
+CODEC_DICT = {
+    "default": {
+        "name": "Default Meta",
+    },
+}
+
+
+default_values = {
+    "debug": 0,
+    "dry_run": False,
+    "cleanup": 1,
+    "codec_list": ",".join(str(k) for k in CODEC_DICT.keys()),
+    "workdir": tempfile.gettempdir(),
+    "add_average": True,
+    "infile_list": None,
+    "outfile": None,
+}
+
+
+COLUMN_LIST = [
+    "infile",
+    "width",
+    "height",
+    "depth",
+    "codec",
+    "raw_size",
+    "encoded_size",
+    "encoded_bpp",
+    "encoded_cr",
+]
+
+
+def get_average_results(df):
+    # import the results
+    new_df = pd.DataFrame(columns=list(df.columns.values))
+    for codec in list(df["codec"].unique()):
+        # select interesting data
+        tmp_df = df[(df["codec"] == codec)]
+        if tmp_df.size == 0:
+            # no entries with this (codec, ) combo
+            continue
+        # start with empty data
+        derived_dict = {key: np.nan for key in list(df.columns.values)}
+        derived_dict["infile"] = "average"
+        derived_dict["codec"] = codec
+        # average a few columns
+        COLUMNS_MEAN = (
+            "width",
+            "height",
+            "depth",
+            "raw_size",
+            "encoded_size",
+            "encoded_bpp",
+            "encoded_cr",
+        )
+        for key in COLUMNS_MEAN:
+            derived_dict[key] = tmp_df[key].mean()
+        new_df.loc[new_df.size] = list(derived_dict.values())
+    return new_df
+
+
+def process_data(
+    infile_list,
+    codec_list,
+    workdir,
+    outfile,
+    add_average,
+    cleanup,
+    debug,
+):
+    df = pd.DataFrame(columns=COLUMN_LIST)
+
+    # run all the input files
+    block_size = 8
+    depth = 8
+    for infile, codec in itertools.product(infile_list, codec_list):
+        # 1. encode the file into an alpha channel
+        alpha_file = tempfile.NamedTemporaryFile(
+            prefix=f"itools-alpha-enctools.codec_{codec}.", suffix=".alpha"
+        ).name
+        itools_alpha.encode_file(infile, alpha_file, codec, block_size, debug)
+        # 2. decode the alpha file
+        out_y4m_file = tempfile.NamedTemporaryFile(
+            prefix=f"itools-alpha-enctools.codec_{codec}.", suffix=".alpha.y4m"
+        ).name
+        itools_alpha.decode_file(alpha_file, out_y4m_file, debug)
+        # 3. check the error
+        outyvu1, _, _, _ = itools_y4m.read_y4m(
+            infile,
+            output_colorrange=itools_common.ColorRange.full,
+            logfd=None,
+            debug=debug,
+        )
+        yarray1 = outyvu1[:, :, 0]
+        height1, width1 = yarray1.shape
+        outyvu2, _, _, _ = itools_y4m.read_y4m(
+            out_y4m_file,
+            output_colorrange=itools_common.ColorRange.full,
+            logfd=None,
+            debug=debug,
+        )
+        yarray2 = outyvu2[:, :, 0]
+        height2, width2 = yarray2.shape
+        assert np.all((yarray1 - yarray2) == 0), f"error: non-zero error in {infile}"
+        # 4. calculate results
+        raw_size = os.path.getsize(infile)
+        encoded_size = os.path.getsize(alpha_file)
+        encoded_bpp = 8.0 * encoded_size / (width1 * height1)
+        encoded_cr = raw_size / encoded_size
+        df.loc[df.size] = (
+            infile,
+            width1,
+            height1,
+            depth,
+            codec,
+            raw_size,
+            encoded_size,
+            encoded_bpp,
+            encoded_cr,
+        )
+
+    # 2. get average results
+    if add_average:
+        derived_df = get_average_results(df)
+        df = pd.concat([df, derived_df], ignore_index=True, axis=0)
+
+    # 4. write the results
+    df.to_csv(outfile, index=False)
+
+
+def get_options(argv):
+    """Generic option parser.
+
+    Args:
+        argv: list containing arguments
+
+    Returns:
+        Namespace - An argparse.ArgumentParser-generated option object
+    """
+    # init parser
+    # usage = 'usage: %prog [options] arg1 arg2'
+    # parser = argparse.OptionParser(usage=usage)
+    # parser.print_help() to get argparse.usage (large help)
+    # parser.print_usage() to get argparse.usage (just usage line)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=itools_version.__version__,
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="count",
+        dest="debug",
+        default=default_values["debug"],
+        help="Increase verbosity (use multiple times for more)",
+    )
+    parser.add_argument(
+        "--quiet",
+        action="store_const",
+        dest="debug",
+        const=-1,
+        help="Zero verbosity",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        dest="dry_run",
+        default=default_values["dry_run"],
+        help="Dry run",
+    )
+    parser.add_argument(
+        "--add-average",
+        action="store_true",
+        dest="add_average",
+        default=default_values["add_average"],
+        help="Add average%s" % (" [default]" if default_values["add_average"] else ""),
+    )
+    parser.add_argument(
+        "--no-add-average",
+        action="store_false",
+        dest="add_average",
+        help="Do not add average%s"
+        % (" [default]" if not default_values["add_average"] else ""),
+    )
+    parser.add_argument(
+        "--cleanup",
+        action="store_const",
+        dest="cleanup",
+        const=1,
+        default=default_values["cleanup"],
+        help="Cleanup Raw Files%s"
+        % (" [default]" if default_values["cleanup"] == 1 else ""),
+    )
+    parser.add_argument(
+        "--full-cleanup",
+        action="store_const",
+        dest="cleanup",
+        const=2,
+        default=default_values["cleanup"],
+        help="Cleanup All Files%s"
+        % (" [default]" if default_values["cleanup"] == 2 else ""),
+    )
+    parser.add_argument(
+        "--no-cleanup",
+        action="store_const",
+        dest="cleanup",
+        const=0,
+        help="Do Not Cleanup Files%s"
+        % (" [default]" if not default_values["cleanup"] == 0 else ""),
+    )
+    parser.add_argument(
+        "--codec",
+        action="store",
+        type=str,
+        dest="codec_list",
+        default=default_values["codec_list"],
+        choices=list(CODEC_DICT.keys()),
+        metavar="[%s]"
+        % (
+            " | ".join(
+                list(CODEC_DICT.keys()),
+            )
+        ),
+        help="codec",
+    )
+    parser.add_argument(
+        "--codec-list",
+        dest="show_codec_list",
+        action="store_true",
+        default=False,
+        help="List available codecs and exit",
+    )
+    parser.add_argument(
+        "--workdir",
+        action="store",
+        dest="workdir",
+        type=str,
+        default=default_values["workdir"],
+        metavar="Work directory",
+        help="work directory",
+    )
+    parser.add_argument(
+        dest="infile_list",
+        type=str,
+        nargs="+",
+        default=default_values["infile_list"],
+        metavar="input-file-list",
+        help="input file list",
+    )
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        action="store",
+        dest="outfile",
+        type=str,
+        default=default_values["outfile"],
+        metavar="output-file",
+        help="output file",
+    )
+    # do the parsing
+    options = parser.parse_args(argv[1:])
+    # parse quick options
+    if options.show_codec_list:
+        print(f"list of valid codecs: {CODEC_LIST}")
+        sys.exit()
+    return options
+
+
+def main(argv):
+    # parse options
+    options = get_options(argv)
+    # set workdir
+    if options.workdir is not None:
+        os.makedirs(options.workdir, exist_ok=True)
+        tempfile.tempdir = options.workdir
+    # get outfile
+    if options.outfile is None or options.outfile == "-":
+        options.outfile = "/dev/fd/1"
+    # print results
+    if options.debug > 0:
+        print(f"debug: {options}")
+    # fix comma-separated lists
+    options.codec_list = options.codec_list.split(",")
+    options.codec_list.sort()
+    # process infile
+    process_data(
+        options.infile_list,
+        options.codec_list,
+        options.workdir,
+        options.outfile,
+        options.add_average,
+        options.cleanup,
+        options.debug,
+    )
+
+
+if __name__ == "__main__":
+    # at least the CLI program name: (CLI) execution
+    main(sys.argv)
